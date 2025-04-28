59a548ab676747a025911f4580e858e9b8e014db
romeomanoela <romeomanoela18@gmail.com>
2025-04-28 21:07:30 +0300

    responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,35 @@ function Home() {
   return (
     <div className='container mx-auto'>
       <h1
-        className='numeric glitch-effect text-center text-3xl tracking-[0.8rem] uppercase'
+        className='numeric glitch-effect text-center text-2xl tracking-[0.2rem] uppercase md:text-3xl md:tracking-[0.8rem]'
         data-text='École ingénieuse, la pépinière des élites informaticiennes'
       >
         École ingénieuse, la pépinière des élites informaticiennes
       </h1>
 
-      <div className='mt-12 flex justify-center'>
-        <p className='typing-effect numeric max-w-[80%] text-xl text-lime-400'>
+      <div className='mt-8 flex justify-center'>
+        <p className='typing-effect numeric max-w-[85%] text-lime-400 md:max-w-[60%] md:text-2xl md:tracking-[0.4rem]'>
           &gt; Bienvenue dans le futur de l&#39;informatique...
         </p>
       </div>
-      <div className={'mt-16 grid grid-cols-5 gap-4'}>
+      <div className={'mt-8 md:mt-16 md:grid md:grid-cols-5 md:gap-4'}>
         <Img
           src={'/eni-2.jpg'}
           alt={'eni-2'}
           width={900}
           height={700}
-          className={'col-span-2 rounded-2xl'}
+          className={'rounded-2xl md:col-span-2'}
         />
-        <div className={'col-span-3 ml-8'}>
-          <h2 className={'mb-8 text-2xl tracking-widest text-lime-400 md:text-4xl'}>
+        <div className={'mt-4 w-full md:col-span-3 md:mt-0 md:ml-8'}>
+          <h2
+            className={'mb-4 text-xl tracking-widest text-lime-400 md:mb-8 md:text-2xl lg:text-4xl'}
+          >
             L&#39;<span className={'text-white'}>E</span>cole{' '}
             <span className={'text-white'}>N</span>ationale d&#39;
             <span className={'text-white'}>I</span>nformatique, votre passerelle vers l&#39;avenir
             de la technologie
           </h2>
-          <p className={'text-xl'}>
+          <p className={'text-base md:text-xl'}>
             Située au cœur de Fianarantsoa, notre université est dédiée à l&#39;excellence dans
             l&#39;enseignement de l&#39;informatique et à la formation de professionnels compétents
             et innovants. L&#39;Ecole Nationale d&#39;Informatique, en abrégé ENI, est un
@@ -39,12 +41,14 @@ function Home() {
           </p>
         </div>
       </div>
-      <div className={'mt-20 grid grid-cols-5 gap-4'}>
-        <div className={'col-span-3'}>
-          <h1 className={'mb-8 text-2xl tracking-widest text-lime-400 uppercase md:text-4xl'}>
+      <div className={'mt-12 flex flex-col-reverse md:grid md:grid-cols-5 md:gap-4'}>
+        <div className={'mt-6 md:col-span-3 md:mt-0'}>
+          <h1
+            className={'mb-4 text-2xl tracking-widest text-lime-400 uppercase md:mb-8 md:text-4xl'}
+          >
             Mission et vision
           </h1>
-          <p className={'text-xl'}>
+          <p className={'text-base md:text-xl'}>
             Notre mission est de former une génération de professionnels de l&#39;informatique
             créatifs et éthiques, prêts à exceller dans un monde numérique en constante évolution.
             Nous nous engageons à contribuer au développement durable de Madagascar en offrant une
@@ -55,11 +59,11 @@ function Home() {
             progrès socio-économique de notre nation et du monde.
           </p>
         </div>
-        <div className={'col-span-2'}>
+        <div className={'md:col-span-2'}>
           <Img
             src={'/directeur.jpg'}
             alt={'directeur'}
-            className={'rounded-2xl'}
+            className={'w-full rounded-2xl'}
             width={900}
             height={700}
           />
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -3,29 +3,45 @@ import Link from 'next/link'
 
 function Header() {
   return (
-    <header className={'flex items-center justify-between px-4'}>
+    <header className={'mb-8 flex items-center justify-between px-4'}>
       <Link href={'/'}>
-        <Img src='/logo.png' alt='logo-eni' width={57} height={57} />
+        <Img
+          src='/logo.png'
+          alt='logo-eni'
+          width={57}
+          height={57}
+          className={'h-[40px] w-[40px] md:h-[55px] md:w-[55px]'}
+        />
       </Link>
       <nav
         className={
-          'absolute top-0 left-1/2 z-10 -translate-x-1/2 bg-white' +
-          ' flex justify-center gap-6 px-10 py-2 text-sm font-semibold text-black'
+          'absolute top-0 left-1/2 z-10 -translate-x-1/2 bg-black/80 backdrop-blur-sm' +
+          ' flex justify-center gap-2 px-5 py-2 text-sm font-semibold text-lime-400 md:gap-6 md:px-12' +
+          ' border-x border-b border-lime-900/50 md:text-base'
         }
         style={{
           clipPath: 'polygon(0% 0%, 100% 0%, 90% 100%, 15% 100%)',
         }}
       >
-        <Link href={'/'}>Accueil</Link>
-        <Link href={'/enseignants'}>Enseignants</Link>
-        <Link href={'/mentions-parcours'}>Mentions et parcours</Link>
+        <Link href={'/'} className='transition-colors duration-300 hover:text-white'>
+          Accueil
+        </Link>
+        <Link href={'/enseignants'} className='transition-colors duration-300 hover:text-white'>
+          Enseignants
+        </Link>
+        <Link
+          href={'/mentions-parcours'}
+          className='transition-colors duration-300 hover:text-white'
+        >
+          Parcours
+        </Link>
       </nav>
       <Link
         href={'/galerie'}
         className={
           'rounded-3xl border border-lime-800' +
-          ' px-4 transition-all hover:border-white hover:text-lime-800' +
-          ' duration-300'
+          ' px-1 transition-all hover:border-white hover:text-lime-800 md:px-4' +
+          ' text-sm duration-300 md:text-base'
         }
       >
         Galerie
